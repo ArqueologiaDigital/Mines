59abba50c72a235c3196144ee8e3deb56926b72b
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2022-05-30 03:28:58 -0300

    [ncurses] draw_minefield with colors

--- common/minefield.c
+++ common/minefield.c
@@ -1,10 +1,3 @@
-// TODO:
-// Definir aqui uma estrutura de dados para representar o campo de jogo
-// Um método para inicializar o campo de jogo com as dimensoes requisitadas
-//
-// Nota: Precisaremos suportar tamanhos diferentes de campo para poder se adaptar
-//       às resoluções específicas de cada plataforma.
-
 #include "main.h"
 #include "minefield.h"
 
