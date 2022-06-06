249fd0b3212782d74eb909c8adc0a0cdd410c630
Pedro de Medeiros <pedro.medeiros@gmail.com>
2022-06-06 14:37:47 -0300

    [MSX] basic video functions

--- platforms/msx2/msx.h
+++ platforms/msx2/msx.h
@@ -10,4 +10,22 @@
 #define SDCCCALL
 #endif
 
+/* fake tile-based screen used by set_char */
+#define SCREEN_WIDTH 32
+#define SCREEN_HEIGHT 25
+
+void write_vdp(uint8_t reg, uint8_t data) SDCCCALL;
+
+void write_vram(uint8_t *dst, uint16_t len, uint8_t *src) SDCCCALL;
+
+void fill_vram(uint8_t byte, uint16_t len, uint8_t *dst) SDCCCALL;
+
+void set_colors(uint8_t fg, uint8_t bg, uint8_t bd) SDCCCALL;
+
+void enable_screen();
+
+void disable_screen();
+
+void lmmm(uint16_t sx, uint16_t sy, uint16_t dx, uint16_t dy, uint16_t width, uint16_t height) SDCCCALL;
+
 #endif /* MSX_H */
