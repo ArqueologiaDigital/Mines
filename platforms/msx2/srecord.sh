e12f43bdf4f902900551b36865b19768448d2fb7
Pedro de Medeiros <pedro.medeiros@gmail.com>
2022-06-07 00:02:59 -0300

    New colorful tiles created by @mrparn

--- platforms/msx2/srecord.sh
+++ platforms/msx2/srecord.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# MINES.SC5 generated from mines.png by https://msx.jannone.org/conv/
+dd if=MINES.SC5 of=mines.dat skip=7 bs=1
 srec_cat mines.dat -binary -o mines.tmp -asm -hexadecimal_style
 sed s/DB/\.db/ mines.tmp > mines.z80
 rm mines.tmp
