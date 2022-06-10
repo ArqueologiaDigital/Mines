ab8b1978342f6ade13e29d9fd085887dd41ced8d
L. Pereira <l@tia.mat.br>
2022-06-09 22:04:58 -0700

    Add fuzzing platform
    This allows us to test the game engine to find crashes.  Requires
    the Clang compiler (for the Fuzzer Sanitizer).  Instructions in
    README.md file.

--- common/main.h
+++ common/main.h
@@ -5,3 +5,4 @@ void idle_loop(minefield* mf);
 void draw_minefield(minefield* mf);
 void draw_minefield_contents(minefield* mf);
 void shutdown();
+void main_gameplay_loop(minefield* mf);
---
+++ platforms/fuzz/input.c
@@ -0,0 +1,70 @@
+#include "main.h"
+#include "common.h"
+#include "minefield.h"
+#include <stdlib.h>
+
+static const uint8_t *fuzz_data, *fuzz_data_end;
+
+uint8_t input_read(uint8_t source)
+{
+	if (fuzz_data < fuzz_data_end) {
+		uint8_t ret = *fuzz_data;
+
+		fuzz_data++;
+
+		switch (ret) {
+		case 'L':
+			return MINE_INPUT_LEFT;
+		case 'R':
+			return MINE_INPUT_RIGHT;
+		case 'U':
+			return MINE_INPUT_UP;
+		case 'D':
+			return MINE_INPUT_DOWN;
+		case '\n':
+			return MINE_INPUT_OPEN;
+		case 'B':
+			return MINE_INPUT_OPEN_BLOCK;
+		case 'F':
+			return MINE_INPUT_FLAG;
+		case 'Q':
+			return MINE_INPUT_QUIT;
+		}
+	}
+
+	return MINE_INPUT_IGNORED;
+}
+
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+	fuzz_data = data;
+	fuzz_data_end = data + size;
+
+	minefield mf;
+
+	setup_minefield(&mf, 10, 10, 5);
+	mf.state = PLAYING_GAME;
+
+	while (mf.state != QUIT)
+		main_gameplay_loop(&mf);
+
+	return 0;
+}
+
+int random_number(int min_num, int max_num)
+{
+	int result = 0, low_num = 0, hi_num = 0;
+
+	if (min_num < max_num)
+	{
+		low_num = min_num;
+		hi_num = max_num + 1; // include max_num in output
+	} else {
+		low_num = max_num + 1; // include max_num in output
+		hi_num = min_num;
+	}
+
+	result = (rand() % (hi_num - low_num)) + low_num;
+	return result;
+}
