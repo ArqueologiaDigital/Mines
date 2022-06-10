b5f3902354f30db63c5970f785464bdf6ba1b3d4
Pedro de Medeiros <pedro.medeiros@gmail.com>
2022-06-10 00:02:28 -0300

    [MSX] MSXbin converts binary files into C or ASM source code

--- platforms/msx2/srecord.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# MINES.SC5 generated from mines.png by https://msx.jannone.org/conv/
-dd if=MINES.SC5 of=mines.dat skip=7 bs=1
-srec_cat mines.dat -binary -o mines.c -c-array mines_data -hexadecimal_style
-rm mines.dat
---
+++ platforms/msx2/tools/MSXtk_types.h
@@ -0,0 +1,48 @@
+//_____________________________________________________________________________
+//   ▄▄   ▄ ▄  ▄▄▄ ▄▄ ▄   ▄▄▄▄           ▄▄         ▄▄ ▄ ▄  ▄▄
+//  ██ ▀ ██▀█ ▀█▄  ▀█▄▀    ██  ▄█▀▄ ▄█▀▄ ██   ██▀   ██▄▀ ▄  ██▀
+//  ▀█▄▀ ██ █ ▄▄█▀ ██ █    ██  ▀█▄▀ ▀█▄▀ ▀█▄ ▄██    ██ █ ██ ▀█▄
+//_____________________________________________________________________________
+//
+// by Guillaume "Aoineko" Blanchard (aoineko@free.fr)
+// available on GitHub (https://github.com/aoineko-fr/CMSXtk)
+// under CC-BY-AS license (https://creativecommons.org/licenses/by-sa/2.0/)
+//_____________________________________________________________________________
+#pragma once
+
+//-----------------------------------------------------------------------------
+// integer types
+typedef char               i8;  ///<  8 bits integer
+typedef short              i16; ///< 16 bits integer
+typedef long               i32; ///< 32 bits integer
+typedef long long          i64; ///< 64 bits integer
+
+//-----------------------------------------------------------------------------
+// unsigned integer types
+typedef unsigned char      u8;  ///<  8 bits unsigned integer
+typedef unsigned short     u16; ///< 16 bits unsigned integer
+typedef unsigned long      u32; ///< 32 bits unsigned integer
+typedef unsigned long long u64; ///< 64 bits unsigned integer
+
+//-----------------------------------------------------------------------------
+// signed integer types
+typedef signed char        s8;	///<  8 bits signed integer
+typedef signed short       s16;	///< 16 bits signed integer
+typedef signed long        s32;	///< 32 bits signed integer
+typedef signed long long   s64;	///< 64 bits signed integer
+
+//-----------------------------------------------------------------------------
+// float types
+typedef float              f32; ///< 32 bits floating-point
+typedef double             f64; ///< 64 bits floating-point
+
+//-----------------------------------------------------------------------------
+// character types
+typedef char               c8;	///<  8 bits character
+typedef wchar_t            c16;	///< 16 bits character
+
+//-----------------------------------------------------------------------------
+// help macros
+#define numberof(tab)		(sizeof(tab) / sizeof(tab[0]))
+#define loop(var, x)		for(u32 var = 0; var < x; var++)
+#define loopx(x)			for(u32 i##__LINE__ = 0; i##__LINE__ < x; i##__LINE__++)
