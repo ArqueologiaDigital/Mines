4f06efdb705f44a03f177a2f5771b7c6839e20ed
L. Pereira <l@tia.mat.br>
2022-06-21 11:23:57 -0700

    [common] Reuse more calculations in draw_minefield_contents()

--- common/video-tiles.c
+++ common/video-tiles.c
@@ -30,105 +30,86 @@ void draw_single_cell(minefield *mf, uint8_t x, uint8_t y)
     }
 }
 
-void draw_minefield_contents(minefield* mf)
+void draw_minefield_contents(minefield *mf)
 {
-	for (uint8_t x = 0; x < mf->width; x++) {
-		for (uint8_t y = 0; y < mf->height; y++) {
-			draw_single_cell(mf, x, y);
-		}
-	}
+    for (uint8_t x = 0; x < mf->width; x++) {
+        for (uint8_t y = 0; y < mf->height; y++) {
+            draw_single_cell(mf, x, y);
+        }
+    }
 
-	uint8_t x = CURRENT_CELL_X(mf);
-	uint8_t y = CURRENT_CELL_Y(mf);
-	highlight_cell(mf, MINEFIELD_X_OFFSET + x * 2 + 1,
-	               MINEFIELD_Y_OFFSET + y * 2 + 1);
+    uint8_t x = CURRENT_CELL_X(mf);
+    uint8_t y = CURRENT_CELL_Y(mf);
+    highlight_cell(mf, MINEFIELD_X_OFFSET + x * 2 + 1,
+                   MINEFIELD_Y_OFFSET + y * 2 + 1);
 }
 
-
-void draw_minefield(minefield* mf)
+void draw_minefield(minefield *mf)
 {
     for (uint8_t x = 0; x <= mf->width; x++) {
         for (uint8_t y = 0; y <= mf->height; y++) {
+            uint8_t xx = MINEFIELD_X_OFFSET + x * 2;
+            uint8_t yy = MINEFIELD_Y_OFFSET + y * 2 - 1;
             if (y > 0 && y <= mf->height) {
                 if (x == 0) {
-                    set_tile(MINEFIELD_X_OFFSET + x * 2,
-                             MINEFIELD_Y_OFFSET + y * 2 - 1,
-                             MINEFIELD_VERTICAL_LEFT);
+                    set_tile(xx, yy, MINEFIELD_VERTICAL_LEFT);
                 } else if (x < mf->width) {
-                    set_tile(MINEFIELD_X_OFFSET + x * 2,
-                             MINEFIELD_Y_OFFSET + y * 2 - 1,
-                             MINEFIELD_VERTICAL_MIDDLE);
+                    set_tile(xx, yy, MINEFIELD_VERTICAL_MIDDLE);
                 } else {
-                    set_tile(MINEFIELD_X_OFFSET + x * 2,
-                             MINEFIELD_Y_OFFSET + y * 2 - 1,
-                             MINEFIELD_VERTICAL_RIGHT);
+                    set_tile(xx, yy, MINEFIELD_VERTICAL_RIGHT);
                 }
             }
 
             if (x < mf->width - 1 && y < mf->height - 1)
-                set_tile(MINEFIELD_X_OFFSET + x * 2 + 2,
-                         MINEFIELD_Y_OFFSET + y * 2 + 2,
-                         MINEFIELD_CROSS);
+                set_tile(xx + 2, yy + 3, MINEFIELD_CROSS);
 
             if (x > 0 && x <= mf->width) {
+                xx--;
+                yy++;
                 if (y == 0) {
-                    set_tile(MINEFIELD_X_OFFSET + x * 2 - 1,
-                             MINEFIELD_Y_OFFSET + y * 2,
-                             MINEFIELD_HORIZONTAL_TOP);
-            	} else if (y < mf->height) {
-                    set_tile(MINEFIELD_X_OFFSET + x * 2 - 1,
-                             MINEFIELD_Y_OFFSET + y * 2,
-                             MINEFIELD_HORIZONTAL_MIDDLE);
-            	} else {
-                    set_tile(MINEFIELD_X_OFFSET + x * 2 - 1,
-                             MINEFIELD_Y_OFFSET + y * 2,
-                             MINEFIELD_HORIZONTAL_BOTTOM);
-            	}
+                    set_tile(xx, yy, MINEFIELD_HORIZONTAL_TOP);
+                } else if (y < mf->height) {
+                    set_tile(xx, yy, MINEFIELD_HORIZONTAL_MIDDLE);
+                } else {
+                    set_tile(xx, yy, MINEFIELD_HORIZONTAL_BOTTOM);
+                }
             }
         }
     }
 
-	set_tile(MINEFIELD_X_OFFSET,
-			 MINEFIELD_Y_OFFSET,
-			 MINEFIELD_CORNER_TOP_LEFT);
+    set_tile(MINEFIELD_X_OFFSET, MINEFIELD_Y_OFFSET, MINEFIELD_CORNER_TOP_LEFT);
 
-	set_tile(MINEFIELD_X_OFFSET + mf->width*2,
-			 MINEFIELD_Y_OFFSET,
-			 MINEFIELD_CORNER_TOP_RIGHT);
+    set_tile(MINEFIELD_X_OFFSET + mf->width * 2, MINEFIELD_Y_OFFSET,
+             MINEFIELD_CORNER_TOP_RIGHT);
 
-	set_tile(MINEFIELD_X_OFFSET,
-			 MINEFIELD_Y_OFFSET + mf->height*2,
-			 MINEFIELD_CORNER_BOTTOM_LEFT);
+    set_tile(MINEFIELD_X_OFFSET, MINEFIELD_Y_OFFSET + mf->height * 2,
+             MINEFIELD_CORNER_BOTTOM_LEFT);
 
-	set_tile(MINEFIELD_X_OFFSET + mf->width*2,
-			 MINEFIELD_Y_OFFSET + mf->height*2,
-			 MINEFIELD_CORNER_BOTTOM_RIGHT);
-			 
-	for (uint8_t x = 0; x <= mf->width; x++){
-		if (x > 0 && x < mf->width){
-			set_tile(MINEFIELD_X_OFFSET + x*2,
-					 MINEFIELD_Y_OFFSET,
-					 MINEFIELD_TOP_TEE);
+    set_tile(MINEFIELD_X_OFFSET + mf->width * 2,
+             MINEFIELD_Y_OFFSET + mf->height * 2,
+             MINEFIELD_CORNER_BOTTOM_RIGHT);
 
-			set_tile(MINEFIELD_X_OFFSET + x*2,
-					 MINEFIELD_Y_OFFSET + mf->height*2,
-					 MINEFIELD_BOTTOM_TEE);
-		}
-	}
+    for (uint8_t x = 0; x <= mf->width; x++) {
+        if (x > 0 && x < mf->width) {
+            set_tile(MINEFIELD_X_OFFSET + x * 2, MINEFIELD_Y_OFFSET,
+                     MINEFIELD_TOP_TEE);
 
-	for (uint8_t y = 0; y <= mf->height; y++){
-		if (y > 0 && y < mf->height){
-			set_tile(MINEFIELD_X_OFFSET,
-					 MINEFIELD_Y_OFFSET + y*2,
-					 MINEFIELD_LEFT_TEE);
+            set_tile(MINEFIELD_X_OFFSET + x * 2,
+                     MINEFIELD_Y_OFFSET + mf->height * 2, MINEFIELD_BOTTOM_TEE);
+        }
+    }
 
-			set_tile(MINEFIELD_X_OFFSET + mf->width*2,
-					 MINEFIELD_Y_OFFSET + y*2,
-					 MINEFIELD_RIGHT_TEE);
-		}
-	}
+    for (uint8_t y = 1; y <= mf->height; y++) {
+        if (y < mf->height) {
+            set_tile(MINEFIELD_X_OFFSET, MINEFIELD_Y_OFFSET + y * 2,
+                     MINEFIELD_LEFT_TEE);
 
-	draw_minefield_contents(mf);
+            set_tile(MINEFIELD_X_OFFSET + mf->width * 2,
+                     MINEFIELD_Y_OFFSET + y * 2, MINEFIELD_RIGHT_TEE);
+        }
+    }
+
+    draw_minefield_contents(mf);
 }
 
 
