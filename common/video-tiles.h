afb20e8f365fc6cb0fbb98dd5ab2d6a806802739
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2022-06-24 20:00:26 -0300

    remove/rename/resuse old char-codes/video-tiles files

--- common/video-tiles.h
+++ common/video-tiles.h
@@ -1,10 +1,9 @@
 #ifndef VIDEO_TILES_H
 #define VIDEO_TILES_H
 
-
 #include <stdint.h>
 #include "minefield.h"
-
+#include "video-tile-codes.h"
 
 /**
  * [Required] Set the tile position at the (`x`, `y`) coordinates with `tile`
@@ -52,64 +51,5 @@ void draw_scenario();
  * See [tile_index](#tile_index) for the index value of the `EXPLOSION` tile.
  */
 void highlight_current_cell(minefield* mf);
-
-
-enum {
-    /*    NOTE:
-        The tile codes for the number
-        of bombs *MUST* be sequential
-        because the video code assumes
-        that to be the case.    */
-    ONE_BOMB,
-    TWO_BOMBS,
-    THREE_BOMBS,
-    FOUR_BOMBS,
-    FIVE_BOMBS,
-    SIX_BOMBS,
-    SEVEN_BOMBS,
-    EIGHT_BOMBS,
-    
-    BLANK,
-    CURSOR,
-    BOMB,
-    FLAG,
-    QUESTION_MARK,
-    EXPLOSION,
-    GROUND,
-    MINEFIELD_CORNER_TOP_LEFT,
-    MINEFIELD_TOP_TEE,
-    MINEFIELD_HORIZONTAL_TOP,
-    MINEFIELD_CORNER_TOP_RIGHT,
-
-    CORNER_TOP_LEFT,
-    TOP_BORDER__LEFT,
-    TOP_BORDER__RIGHT,
-    CORNER_TOP_RIGHT,
-    LEFT_BORDER__TOP,
-    RIGHT_BORDER__TOP,
-    LEFT_BORDER__BOTTOM,
-    RIGHT_BORDER__BOTTOM,
-    CORNER_BOTTOM_LEFT,
-    BOTTOM_BORDER__LEFT,
-    BOTTOM_BORDER__RIGHT,
-    CORNER_BOTTOM_RIGHT,
-
-    MINEFIELD_LEFT_TEE,
-    MINEFIELD_CROSS,
-    MINEFIELD_HORIZONTAL_MIDDLE,
-    MINEFIELD_VERTICAL_MIDDLE,
-    MINEFIELD_RIGHT_TEE,
-    MINEFIELD_VERTICAL_LEFT,
-    CLOSED_CELL,
-    MINEFIELD_VERTICAL_RIGHT,
-    MINEFIELD_CORNER_BOTTOM_LEFT,
-    MINEFIELD_BOTTOM_TEE,
-    MINEFIELD_HORIZONTAL_BOTTOM,
-    MINEFIELD_CORNER_BOTTOM_RIGHT,
-
-    /* Insert new tiles here */
-
-    MAX_VIDEO_TILES,
-};
 
 #endif /* VIDEO_TILES_H */
