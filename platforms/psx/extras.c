31c4946497578add35a90cd08a5818a0edd33f7b
Vivi Macedo <7549205+vinicyusmacedo@users.noreply.github.com>
2022-06-26 11:31:27 -0300

    [psx] implemented debug, tidying up display functions

--- platforms/psx/extras.c
+++ platforms/psx/extras.c
@@ -1,4 +1,6 @@
 #include "../../common/game.h"
+#include <psxsio.h>
+#include <stdio.h>
 
 minefield* init_minefield()
 {
@@ -20,11 +22,22 @@ void free_minefield(minefield* mf)
     free(mf);
 }
 
+void debug(char* msg, uint8_t value) {
+    printf("%s%d\n", msg, value);
+}
+
 void platform_init() {
     int counter;
 
     // Init stuff   
     init_gl();
     start_pad();
+
+    #ifdef USE_DEBUG_MODE
+    AddSIO(115200);
+    printf("Using debug mode...\n");
+    #endif
 }
 
+// We don't need to implement that :)
+void platform_shutdown() {}
--- platforms/psx/video.c
+++ platforms/psx/video.c
@@ -4,10 +4,10 @@
 #include <psxgte.h>
 #include <psxgpu.h>
 #include "../../common/video-tiles.h"
+#include "../../common/common.h"
 
 #define OTLEN 8
 
-// Define display/draw environments for double buffering
 DISPENV disp[2];
 DRAWENV draw[2];
 int db;
@@ -15,30 +15,22 @@ int db;
 u_long ot[2][OTLEN];
 char pribuff[2][32768];
 char *nextpri;
+TILE *tile;
 
 void init_gl() {
-    // This not only resets the GPU but it also installs the library's
-    // ISR subsystem to the kernel
     ResetGraph(0);
     
-    // Define display environments, first on top and second on bottom
-    SetDefDispEnv(&disp[0], 0, 0, 320, 240);
-    SetDefDispEnv(&disp[1], 0, 240, 320, 240);
+    SetDefDispEnv(&disp[0], 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    SetDefDispEnv(&disp[1], 0, SCREEN_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT);
     
-    // Define drawing environments, first on bottom and second on top
-    SetDefDrawEnv(&draw[0], 0, 240, 320, 240);
-    SetDefDrawEnv(&draw[1], 0, 0, 320, 240);
+    SetDefDrawEnv(&draw[0], 0, SCREEN_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT);
+    SetDefDrawEnv(&draw[1], 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
     
-    // Set and enable clear color
     setRGB0(&draw[0], 0, 96, 0);
     setRGB0(&draw[1], 0, 96, 0);
     draw[0].isbg = 1;
     draw[1].isbg = 1;
     
-    // Clear double buffer counter
-    db = 0;
-    
-    // Apply the GPU environments
     PutDispEnv(&disp[db]);
     PutDrawEnv(&draw[db]);
 
@@ -49,42 +41,34 @@ void init_gl() {
 	FntOpen(0, 8, 320, 224, 0, 100);
 }
 
-// Display function
 void display()
 {
-    // Flip buffer index
-    db = !db;
-    nextpri = pribuff[db];
-    
-    // Wait for all drawing to complete
     DrawSync(0);
-    
-    // Wait for vertical sync to cap the logic to 60fps (or 50 in PAL mode)
-    // and prevent screen tearing
     VSync(0);
 
-    // Switch pages 
     PutDispEnv(&disp[db]);
     PutDrawEnv(&draw[db]);
     
-    // Enable display output, ResetGraph() disables it by default
     SetDispMask(1);
 
-    DrawOTag(ot[db]+OTLEN-1);
+    DrawOTag(ot[db] + OTLEN - 1);
+
+    db = !db;
+    nextpri = pribuff[db];
 }
 
-void platform_shutdown() {
-}
+void platform_shutdown();
 
 void set_tile(uint8_t x, uint8_t y, uint8_t tile_uint) {
-    TILE *tile;
+    debug("x axis = ", x);
+    debug("y axis = ", y);
 
     ClearOTagR(ot[db], OTLEN);
     tile = (TILE*)nextpri;
 
     setTile(tile);
-    setXY0(tile, x, y);
-    setWH(tile, 32, 32);
+    setXY0(tile, x , y);
+    setWH(tile, 10, 10);
     setRGB0(tile, 255, 255, 0);
 
     addPrim(ot[db], tile);
