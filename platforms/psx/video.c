21e89c9b892eda76a3cf2de2876da77c423a5db8
Vivi Macedo <7549205+vinicyusmacedo@users.noreply.github.com>
2022-06-26 20:54:22 -0300

    [psx] grid is now showing up

--- platforms/psx/video.c
+++ platforms/psx/video.c
@@ -7,6 +7,8 @@
 #include "../../common/common.h"
 
 #define OTLEN 8
+#define TILESIZE 10
+#define TILESKIP 9
 
 DISPENV disp[2];
 DRAWENV draw[2];
@@ -15,7 +17,6 @@ int db;
 u_long ot[2][OTLEN];
 char pribuff[2][32768];
 char *nextpri;
-TILE *tile;
 
 void init_gl() {
     ResetGraph(0);
@@ -26,10 +27,10 @@ void init_gl() {
     SetDefDrawEnv(&draw[0], 0, SCREEN_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT);
     SetDefDrawEnv(&draw[1], 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
     
-    setRGB0(&draw[0], 0, 96, 0);
-    setRGB0(&draw[1], 0, 96, 0);
-    draw[0].isbg = 1;
-    draw[1].isbg = 1;
+    //setRGB0(&draw[0], 0, 96, 0);
+    //setRGB0(&draw[1], 0, 96, 0);
+    //draw[0].isbg = 1;
+    //draw[1].isbg = 1;
     
     PutDispEnv(&disp[db]);
     PutDrawEnv(&draw[db]);
@@ -54,20 +55,50 @@ void display()
     DrawOTag(ot[db] + OTLEN - 1);
 
     db = !db;
+
+    DrawOTag(ot[db] + OTLEN - 1);
     nextpri = pribuff[db];
 }
 
-void set_tile(uint8_t x, uint8_t y, uint8_t tile_uint) {
-    debug("x axis = ", x);
-    debug("y axis = ", y);
-
+void set_tile(uint8_t x, uint8_t y, uint8_t tile_type) {
+    TILE *tile;
     ClearOTagR(ot[db], OTLEN);
     tile = (TILE*)nextpri;
 
     setTile(tile);
-    setXY0(tile, x , y);
-    setWH(tile, 10, 10);
-    setRGB0(tile, 255, 255, 0);
+    setXY0(tile, x * TILESKIP, y * TILESKIP);
+    setWH(tile, TILESIZE, TILESIZE);
+
+    switch (tile_type) {
+        case MINEFIELD_CORNER_TOP_LEFT: 
+        case MINEFIELD_CORNER_TOP_RIGHT:
+        case MINEFIELD_CORNER_BOTTOM_LEFT:
+        case MINEFIELD_CORNER_BOTTOM_RIGHT:
+            setRGB0(tile, 0, 63, 0);
+            break;
+        case MINEFIELD_TOP_TEE:
+        case MINEFIELD_BOTTOM_TEE:
+        case MINEFIELD_LEFT_TEE:
+        case MINEFIELD_RIGHT_TEE:
+            setRGB0(tile, 0, 63, 0);
+            break;
+        case MINEFIELD_VERTICAL_LEFT:
+        case MINEFIELD_VERTICAL_MIDDLE:
+        case MINEFIELD_VERTICAL_RIGHT:
+            setRGB0(tile, 0, 63, 0);
+            break;
+        case MINEFIELD_HORIZONTAL_TOP:
+        case MINEFIELD_HORIZONTAL_MIDDLE:
+        case MINEFIELD_HORIZONTAL_BOTTOM:
+            setRGB0(tile, 0, 63, 0);
+            break;
+        case CURSOR:
+            setRGB0(tile, 255, 255, 0);
+            break;
+        default:
+            setRGB0(tile, 31, 128, 0);
+            break;
+    }
 
     addPrim(ot[db], tile);
 
@@ -75,7 +106,25 @@ void set_tile(uint8_t x, uint8_t y, uint8_t tile_uint) {
     display();
 }
 
-void highlight_current_cell(minefield* mf) {}
+void highlight_current_cell(minefield* mf) {
+    uint8_t x = CELL_X(mf, mf->current_cell);
+    uint8_t x2 = x * 2 + MINEFIELD_X_OFFSET + 1;
+    uint8_t y = CELL_Y(mf, mf->current_cell);
+    uint8_t y2 = y * 2 + MINEFIELD_Y_OFFSET + 1;
+    static uint8_t old_x = 0;
+    static uint8_t old_y = 0;
+
+    // remove old cursor position
+    draw_single_cell(mf, old_x, old_y);
+
+    if (mf->state == GAME_OVER)
+        set_tile(x2, y2, EXPLOSION);
+    else
+        set_tile(x2, y2, CURSOR);
+
+    old_x = x;
+    old_y = y;
+}
 
 void idle_update(minefield* mf) {}
 
